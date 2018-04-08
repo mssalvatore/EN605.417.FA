941257b0b658e80602501e4a0c72ccfeb7e6d214
Mike Salvatore <mike.s.salvatore@gmail.com>
2018-04-08 12:17:31 -0400

    Break options parsing out into separate file

---
+++ final_project/options.cpp
@@ -0,0 +1,94 @@
+enum BettingStrategy { MARTINGALE, DALEMBERT };
+
+struct ProgramOptions
+{
+    public:
+        int numRuns;
+        int spinsPerRun;
+        float winProbability;
+        int bettingFactor;
+        BettingStrategy bettingStrategy;
+
+        ProgramOptions(int inNumRuns = 1, int inSpinsPerRun = 100, float inWinProbability = .4737, int inBettingFactor = 2, BettingStrategy inBettingStrategy = MARTINGALE): numRuns(inNumRuns), spinsPerRun(inSpinsPerRun), winProbability(inWinProbability), bettingFactor(inBettingFactor), bettingStrategy(inBettingStrategy) {}
+};
+
+class InvalidArgumentException: public std::exception
+{
+    public:
+        InvalidArgumentException(char * argument, std::string type): argument(argument), type(type) {}
+
+        virtual const char* what() const throw()
+        {
+            char *buffer = new char[256];
+            snprintf(buffer, 255, "This provided argument is not a valid %s: %s", this->type.c_str(), this->argument);
+            return buffer;
+        }
+
+    protected:
+        char * argument;
+        std::string type;
+};
+
+int parseIntArgument(char* argument)
+{
+    std::istringstream ss(argument);
+    int x;
+
+    if (!(ss >> x))
+    {
+        throw InvalidArgumentException(argument, "integer");
+    }
+
+    return x;
+}
+
+float parseFloatArgument(char* argument)
+{
+    float x = atof(argument);
+
+    if (x < 0.0 || x > 1.0)
+    {
+        throw InvalidArgumentException(argument, "float between 0.0 and 1.0 inclusive");
+    }
+
+    return x;
+}
+
+ProgramOptions parseOptions(int argc, char* argv[])
+{
+    ProgramOptions options;
+    if (argc >= 2)
+    {
+        options.numRuns = parseIntArgument(argv[1]);
+    }
+    if (argc >= 3)
+    {
+        options.spinsPerRun = parseIntArgument(argv[2]);
+    }
+    if (argc >= 4)
+    {
+        options.winProbability = parseFloatArgument(argv[3]);
+    }
+    if (argc >= 5)
+    {
+        options.bettingFactor = parseIntArgument(argv[4]);
+    }
+    if (argc >= 6)
+    {
+        {
+            if (strcmp(argv[5], "martingale") == 0) 
+            {
+                options.bettingStrategy = MARTINGALE;
+            }
+            else if (strcmp(argv[5], "dalembert") == 0) 
+            {
+                options.bettingStrategy = DALEMBERT;
+            }
+            else {
+                throw InvalidArgumentException(argv[5], "betting strategy");
+            }
+        }
+    }
+
+    return options;
+}
--- final_project/roulette.cu
+++ final_project/roulette.cu
@@ -11,6 +11,8 @@
 #include <curand.h>
 #include <curand_kernel.h>
 
+#include "options.cpp"
+
 #define MAX_THREADS_PER_BLOCK 1024
 #define BLOCK_SIZE MAX_THREADS_PER_BLOCK
 #define WARP_SIZE 32
@@ -156,100 +158,11 @@ void runDalembert(int numRuns, int spinsPerRun, float winProbability)
     printf("It took %d us \n", runTime);
 }
 
-enum BettingStrategy { MARTINGALE, DALEMBERT };
-
-struct ProgramOptions
-{
-    public:
-        int numRuns;
-        int spinsPerRun;
-        float winProbability;
-        int bettingFactor;
-        BettingStrategy bettingStrategy;
-
-        ProgramOptions(int inNumRuns = 1, int inSpinsPerRun = 100, float inWinProbability = .4737, int inBettingFactor = 2, BettingStrategy inBettingStrategy = MARTINGALE): numRuns(inNumRuns), spinsPerRun(inSpinsPerRun), winProbability(inWinProbability), bettingFactor(inBettingFactor), bettingStrategy(inBettingStrategy) {}
-};
-
-class InvalidArgumentException: public std::exception
-{
-    public:
-        InvalidArgumentException(char * argument, std::string type): argument(argument), type(type) {}
-
-        virtual const char* what() const throw()
-        {
-            char *buffer = new char[256];
-            snprintf(buffer, 255, "This provided argument is not a valid %s: %s", this->type.c_str(), this->argument);
-            return buffer;
-        }
-
-    protected:
-        char * argument;
-        std::string type;
-};
-
-int parseIntArgument(char* argument)
-{
-    std::istringstream ss(argument);
-    int x;
-
-    if (!(ss >> x))
-    {
-        throw InvalidArgumentException(argument, "integer");
-    }
-
-    return x;
-}
-
-float parseFloatArgument(char* argument)
-{
-    float x = atof(argument);
-
-    if (x < 0.0 || x > 1.0)
-    {
-        throw InvalidArgumentException(argument, "float between 0.0 and 1.0 inclusive");
-    }
-
-    return x;
-}
-
-
 // Main function
 int main(int argc, char* argv[])
 {
-    ProgramOptions options;
-    if (argc >= 2)
-    {
-        options.numRuns = parseIntArgument(argv[1]);
-    }
-    if (argc >= 3)
-    {
-        options.spinsPerRun = parseIntArgument(argv[2]);
-    }
-    if (argc >= 4)
-    {
-        options.winProbability = parseFloatArgument(argv[3]);
-    }
-    if (argc >= 5)
-    {
-        options.bettingFactor = parseIntArgument(argv[4]);
-    }
-    if (argc >= 6)
-    {
-        {
-            if (strcmp(argv[5], "martingale") == 0) 
-            {
-                options.bettingStrategy = MARTINGALE;
-            }
-            else if (strcmp(argv[5], "dalembert") == 0) 
-            {
-                options.bettingStrategy = DALEMBERT;
-            }
-            else {
-                throw InvalidArgumentException(argv[5], "betting strategy");
-            }
-        }
-    }
-    //runDalembert(1, 100, .4737);
+    ProgramOptions options = parseOptions(argc, argv);
+
     if (options.bettingStrategy == MARTINGALE)
     {
         runMartingale(options.numRuns, options.spinsPerRun, options.winProbability, options.bettingFactor);
